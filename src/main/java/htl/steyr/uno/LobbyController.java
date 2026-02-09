8888e1474cedfc574344fdc8b5566bf0f8e4f4c6
jonas <jfuerlin@htl-steyr.ac.at>
2026-02-09 14:58:19 +0100

    Created lobby.fxml and LobbyController. Added a first design and created valuables and functions for the buttons

---
+++ src/main/java/htl/steyr/uno/LobbyController.java
@@ -0,0 +1,41 @@
+package htl.steyr.uno;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class LobbyController {
+
+    public Button createPartyButton;
+    public Button joinButton;
+    public Button logoutButton;
+    public Label gerneratedPartyCode;
+
+
+    /**
+     * @param actionEvent
+     *
+     * Generats a code für a new game
+     */
+    public void onCreatePartyClicked(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * @param actionEvent
+     *
+     * Joins a new game with all players that are in the party
+     */
+    public void onJoinButtonClicked(ActionEvent actionEvent) {
+
+    }
+
+    /**
+     * @param actionEvent
+     *
+     * Logs the acc out / goes back to the login screen
+     */
+    public void onLogoutButtonClicked(ActionEvent actionEvent) {
+
+    }
+}
