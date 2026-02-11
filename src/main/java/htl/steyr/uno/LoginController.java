4302d9e3de9b17cf70938e27ae00813c248013c3
Moritz <mraschk1@htl-steyr.ac.at>
2026-02-11 14:36:24 +0100

    Add account creation and login functionality to LoginController and update FXML layout

--- src/main/java/htl/steyr/uno/LoginController.java
+++ src/main/java/htl/steyr/uno/LoginController.java
@@ -3,15 +3,43 @@ package htl.steyr.uno;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 public class LoginController {
 
-    @FXML private VBox hideLogin;
+
+    @FXML private Button showLogin;
+    @FXML private VBox showNewAccScreen;
     @FXML private Button createAcc;
+    @FXML private VBox showCreateAcc;
+    @FXML private StackPane loginPane;
+    @FXML private StackPane brandingPane;
+    @FXML private PasswordField newAccPassword;
+    @FXML private TextField newAccUserName;
+    @FXML private TextField newAccLastName;
+    @FXML private TextField newAccFirstName;
+    @FXML private PasswordField welcomeBackPasswd;
+    @FXML private TextField welcomeBackUserName;
 
     @FXML
-    private void onCreateButtonClicked(ActionEvent actionEvent) {
-        hideLogin.setVisible(false);
+    private void onCreateAccountButtonClicked(ActionEvent actionEvent) {
+        showCreateAcc.setVisible(false);
+        showNewAccScreen.setVisible(true);
+        welcomeBackPasswd.clear();
+        welcomeBackUserName.clear();
+    }
+
+
+    @FXML
+    private void backToLogin(ActionEvent actionEvent) {
+        showNewAccScreen.setVisible(false);
+        showCreateAcc.setVisible(true);
+        newAccPassword.clear();
+        newAccUserName.clear();
+        newAccLastName.clear();
+        newAccFirstName.clear();
     }
 }
