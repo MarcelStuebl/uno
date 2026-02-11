96b8b556f9f4616a42163e9509adca978121bd12
BR00TManu <mhoreth@htl-steyr.ac.at>
2026-02-11 13:48:48 +0100

    finished programming Card and Player Class. Classes are not final and can be changed for future use

--- src/main/java/htl/steyr/uno/Player.java
+++ src/main/java/htl/steyr/uno/Player.java
@@ -1,4 +1,29 @@
 package htl.steyr.uno;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Player {
+    private String username;
+    private boolean isCurrentTurn;
+    ArrayList<Card> hand = new ArrayList();
+
+
+
+
+    public ArrayList<Card> getPlayerHand(){
+        return this.hand;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+
+    public void setPlayerHand(ArrayList<Card> arr){
+            this.hand.clear();
+            this.hand.addAll(arr);
+
+    }
+
+
 }
