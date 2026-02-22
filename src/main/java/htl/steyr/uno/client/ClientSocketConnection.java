15d7413724d50db36cae91df6948f191eed05cf3
Marcel <marcel@stuebl.eu>
2026-02-22 22:13:24 +0100

    Add detailed Javadoc comments to ClientSocketConnection methods for improved documentation

--- src/main/java/htl/steyr/uno/client/ClientSocketConnection.java
+++ src/main/java/htl/steyr/uno/client/ClientSocketConnection.java
@@ -20,6 +20,15 @@ public class ClientSocketConnection implements Closeable {
     private User user;
     private LobbyInfoResponse lobby;
 
+
+    /**
+     * Establishes a socket connection to the server and initializes input/output streams.
+     *
+     * @param host   the server's hostname or IP address
+     * @param port   the server's port number
+     * @param client the client instance to handle callbacks
+     * @throws IOException if an I/O error occurs when creating the socket or streams
+     */
     public ClientSocketConnection(String host, int port, Client client) throws IOException {
         this.socket = new Socket(host, port);
         this.client = client;
@@ -29,6 +38,13 @@ public class ClientSocketConnection implements Closeable {
         in = new ObjectInputStream(socket.getInputStream());
     }
 
+
+    /**
+     * Sends a message to the server by writing an object to the output stream.
+     *
+     * @param message the message object to send
+     * @throws RuntimeException if an I/O error occurs while sending the message
+     */
     public void sendMessage(Object message) {
         try {
             out.writeObject(message);
@@ -38,14 +54,24 @@ public class ClientSocketConnection implements Closeable {
         }
     }
 
+
+    /**
+     * Starts a background thread to continuously receive messages from the server.
+     * The thread will process incoming messages and invoke appropriate callbacks on the client.
+     */
     public void startReceiving() {
         running = true;
         receiveThread = new Thread(() -> {
             try {
                 while (running) {
-
                     Object obj = in.readObject();
 
+                    /*
+                     * The following block checks the type of the received message and calls the corresponding
+                     * handler method on the client. Each handler method is responsible for processing a specific
+                     * type of response from the server, such as login success, login failure, lobby information,
+                     * etc. If an unknown message type is received, it will print a message to the console.
+                     */
                     if (obj instanceof LoginSuccessResponse msg) {
                         logInSuccess(msg);
                     } else if (obj instanceof LoginFailedResponse msg) {
@@ -73,6 +99,13 @@ public class ClientSocketConnection implements Closeable {
         receiveThread.start();
     }
 
+
+    /**
+     * Handles a successful login response from the server by updating the user information
+     * and prompting the client to join or create a lobby.
+     *
+     * @param msg the login success response containing user information
+     */
     private void logInSuccess(LoginSuccessResponse msg) {
         this.user = msg.getUser();
 
@@ -83,11 +116,26 @@ public class ClientSocketConnection implements Closeable {
         client.joinOrCreateLobby();
     }
 
+
+    /**
+     * Handles a failed login response from the server by notifying the user and prompting
+     * them to try logging in again.
+     *
+     * @param msg the login failed response containing error information
+     */
     private void logInFailed(LoginFailedResponse msg) {
         System.out.println("Login failed. Please try again.");
         client.logIn();
     }
 
+
+    /**
+     * Handles a lobby information response from the server by updating the lobby state
+     * and displaying the lobby details. If the lobby information is invalid, it prompts
+     * the client to join or create a lobby again.
+     *
+     * @param lobby the lobby information response containing details about the lobby
+     */
     private void gotLobby(LobbyInfoResponse lobby) {
         this.lobby = lobby;
 
@@ -99,11 +147,25 @@ public class ClientSocketConnection implements Closeable {
         }
     }
 
+
+    /**
+     * Handles a lobby not found response from the server by notifying the user and prompting
+     * them to join or create a lobby again.
+     *
+     * @param lobby the lobby not found response indicating that the specified lobby does not exist
+     */
     private void lobbyNotFound(LobbyNotFoundResponse lobby) {
         System.out.println("Invalid lobby ID. Please try again.");
         client.joinOrCreateLobby();
     }
 
+
+    /**
+     * Handles a lobby join refused response from the server by checking the status of the lobby
+     * and notifying the user accordingly. It then prompts the client to join or create a lobby again.
+     *
+     * @param msg the lobby join refused response containing information about why the join request was refused
+     */
     private void lobbyJoinRefused(LobbyJoinRefusedResponse msg) {
         if (msg.getLobbyInfo().getStatus() == 1) {
             System.out.println("Lobby is full. Please try again.");
@@ -115,16 +177,33 @@ public class ClientSocketConnection implements Closeable {
         client.joinOrCreateLobby();
     }
 
+
+    /**
+     * Handles a successful lobby creation response from the server by notifying the user.
+     *
+     * @param msg the create lobby success response indicating that the lobby was created successfully
+     */
     private void createLobbySuccess(CreateLobbySuccessResponse msg) {
         System.out.println("Lobby created successfully.");
     }
 
+
+    /**
+     * Handles a successful lobby join response from the server by notifying the user.
+     *
+     * @param msg the join lobby success response indicating that the lobby was joined successfully
+     */
     private void joinLobbySuccess(JoinLobbySuccessResponse msg) {
         System.out.println("Joined lobby successfully.");
     }
 
+
+    /**
+     * Closes the socket connection and stops the receiving thread. This method is called when the client
+     * is shutting down or when an error occurs that requires closing the connection.
+     */
     @Override
-    public void close() throws IOException {
+    public void close() {
         running = false;
         try {
             socket.close();
