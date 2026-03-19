c5a9bf3a7a543aabf5ef2a622a910d8dbb46d243
marcel <mstuebl@htl-steyr.ac.at>
2026-03-19 12:40:56 +0100

    feat: update drawCard method to throw InvalidCardException for error handling

--- src/main/java/htl/steyr/uno/GameTableClasses/WithdrawalStack.java
+++ src/main/java/htl/steyr/uno/GameTableClasses/WithdrawalStack.java
@@ -1,8 +1,10 @@
 package htl.steyr.uno.GameTableClasses;
 
+import htl.steyr.uno.GameTableClasses.exceptions.InvalidCardException;
+
 public class WithdrawalStack{
 
-    public static Card drawCard(){
+    public static Card drawCard() throws InvalidCardException {
         //here will be the logic for getting 1 singular card from the Server
 
         Card card = new Card(4,"blue"); //card that will be given be the server
