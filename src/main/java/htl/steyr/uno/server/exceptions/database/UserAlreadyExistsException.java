617724437e2fd930466db0e575075570ca799fb4
marcel <mstuebl@htl-steyr.ac.at>
2026-03-24 14:26:23 +0100

    feat: add response handling for account creation failures and improve error messaging

--- src/main/java/htl/steyr/uno/server/exceptions/database/UserAlreadyExistsException.java
+++ src/main/java/htl/steyr/uno/server/exceptions/database/UserAlreadyExistsException.java
@@ -1,7 +1,5 @@
 package htl.steyr.uno.server.exceptions.database;
 
-import htl.steyr.uno.server.exceptions.user.UserException;
-
 public class UserAlreadyExistsException extends UserException {
     public UserAlreadyExistsException(String message) {
         super("UserAlreadyExistsException: " + "User '" + message + "' already exists.");
